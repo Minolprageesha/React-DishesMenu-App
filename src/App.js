b54169dc0f8dc386936dae1dea05749f38f4e785
Your Name <minolprageesha1@gmail.com>
2021-05-30 22:59:24 +0530

    css practices

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
+        <table>
+          <tbody>
+          
+            <tr>
+              <td>
+              <div className = "left">
+                <img className="image" ></img>
+                  <p>this is colom</p>
+                  <table>
+                    <tfoot>
+                      <td className="footer">
+                        4 Read
+                      </td>
+                      <td className="footer">
+                        55 view
+                      </td>
+                      <td className="footer">
+                        77 comments
+                      </td>
+                    </tfoot>
+                  </table>
+                </div>
+
+              </td>
+
+              <td>
+              <div className = "right">
+                  <p>this is colom</p>
+                </div>
+              </td>
+                
+          
+                
+
+            </tr>
+          </tbody>
+        </table>
+        
+        
+
+
+      
+       
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,7 +60,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
